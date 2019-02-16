f583da23c333ae9d4dbef57226c51d33999d6bfb
NancyBolstad <litchacha@gmail.com>
2019-02-16 19:27:58 +0100

    Changed regex

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -5,10 +5,12 @@ function validateForm() {
     var lastName = document.getElementById("last-name").value;
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
-    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    //RFC2822 Email Validation, refer to:https://regexr.com/2rhq7
+    var emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    //Regular expression for Norwegian phone number consisting of 8 digits
     var phoneRegex = /^([0-9]{8})$/;
 
-    //Validate that 'name' and 'last name' inputs are not empty 
+    //Validate that 'name' and 'last name' inputs are not empty. 
     if (firstName == "" || firstName == null) {
         document.getElementById("msg").innerHTML = "First name cannot be empty.";
         document.getElementById("first-name").focus();
@@ -21,19 +23,22 @@ function validateForm() {
         return false;
     }
 
-    //use regex expressions to validate the telephone and email addresses respectively.   
+    //Use regex expressions to validate the telephone and email addresses respectively.   
     if (emailRegex.test(email) == false) {
-        document.getElementById("msg").innerHTML = "Please type in a correct email address";
+        document.getElementById("msg").innerHTML = "Please type in a correct email address.";
         document.getElementById("email").focus();
         return false;
     }  
     
     if (phoneRegex.test(phone) == false) {
-        document.getElementById("msg").innerHTML = "Please type in a correct Norwegian phone number consisted of 8 digits.";
+        document.getElementById("msg").innerHTML = "Please type in a correct Norwegian phone number consisting of 8 digits.";
         document.getElementById("phone").focus();
         return false;
     } 
 
-        alert("Contact information is validated. The form will be submitted to server now.");
-        return true;
+    if (!confirm("Your contact information has been validated. Do you want to submit the form?")){
+        return false;
+    }
+
+    return true;
     }
